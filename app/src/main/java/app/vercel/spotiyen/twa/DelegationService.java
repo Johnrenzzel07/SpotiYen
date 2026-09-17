de6063e28899151bcbf6b312732d51d92b5be18a
Johnrenzzel07 <ronztajanlangit09@gmail.com>
2026-09-17 10:38:39 +0800

    adding build

---
+++ app/src/main/java/app/vercel/spotiyen/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.spotiyen.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
